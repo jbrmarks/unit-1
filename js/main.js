e9718c887e586a5623097bda9024076ae964c896
jbrmarks <jbrmarks@gmail.com>
2019-01-30 23:40:09 -0600

    Module 2 activity

---
+++ js/main.js
@@ -0,0 +1,124 @@
+// Authored by Joe Marks, 2019
+/*eslint-env browser*/
+
+//initialize function called when the script loads
+function initialize(){
+    cities();
+};
+
+// This function adds a city size column with values assigned based
+// on a city's population
+function addColumns(cityPop){
+    // Loop through each table row
+    $('tr').each(function(i){
+        // If this is the first row
+    	if (i == 0){
+            // Create and append a header
+            // ERROR: apend -> append
+    		$(this).append('<th>City Size</th>');
+    	} else {
+            // If this is not the first row
+            // Create a new variable named citySize
+    		var citySize;
+            // Assign a value to citySize based on the city's population
+    		if (cityPop[i-1].population < 100000){
+    			citySize = 'Small';
+    		} else if (cityPop[i-1].population < 500000){
+                // ERROR: citysize -> citySize
+    			citySize = 'Medium';
+    		} else {
+    			citySize = 'Large';
+    		};
+            // Add citySize to the table row
+            // ERROR: $this -> $(this)
+            // ERROR: '<td' -> '<td>'
+    		$(this).append('<td>' + citySize + '</td>');
+    	};
+    });
+};
+
+// This function creates various event listeners with effects
+function addEvents(){
+    // Alias method for the mouseover event listener on 'table'
+    // ERROR: '#table" -> 'table'
+	$('table').mouseover(function(){
+		// Create a variable to hold the rgb color value
+		var color = "rgb(";
+        // For three iterations
+		for (var i=0; i<3; i++){
+            // Assign a random value from 0-255
+			var random = Math.round(Math.random() * 255);
+            // Add this value to our color variable
+            // ERROR: "random" -> random
+			color += random;
+            // If this is one of the first two iterations
+			if (i<2){
+                // Add a comma after the color value
+				color += ",";
+			// If this was the last color value
+			} else {
+                // Add the closed parantheses
+				color += ")";
+            // ERROR: Missing bracket
+            };
+		};
+        // Assign our randomly genereated color to the table
+		$(this).css('color', color);
+	});
+    // A named handler function for a listener
+	function clickme(){
+        // On click, alert message
+		alert('Hey, you clicked me!');
+	};
+    // Add the event listener for 'click' on 'table'
+	$('table').on('click', clickme);
+};
+
+//function to create a table with cities and their populations
+function cities(){
+    //define two arrays for cities and population
+    var cityPop = [
+        { 
+            city: 'Rochester',
+            state: 'MN',
+            population: 115733
+        },
+        {
+            city: 'Rochester',
+            state:'NY',
+            population: 208046
+        },
+        {
+            city: 'Chicago',
+            state:'IL',
+            population: 2716000
+        },
+        {
+            city: 'Minneapolis',
+            state:'MN',
+            population: 422331
+        }
+    ];
+
+    //append the table element to the div
+    $("#mydiv").append("<table>");
+
+    //append a header row to the table
+    $("table").append("<tr>");
+
+    //add the "City" and "Population" columns to the header row
+    $("tr").append("<th>City</th><th>State</th><th>Population</th>");
+
+    //loop to add a new row for each city
+    for (var i = 0; i < cityPop.length; i++){
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].state + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        $("table").append(rowHtml);
+    };
+    addColumns(cityPop);
+    addEvents();
+};
+
+//call the initialize function when the document has loaded
+$(document).ready(initialize);
